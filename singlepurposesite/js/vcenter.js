6436c4045885fcb08bb879bc518794fb7a7c6510
cameverett <cameronjeverett@gmail.com>
2017-03-02 14:37:31 -0500

    Should have done that.

--- singlepurposesite/js/vcenter.js
+++ singlepurposesite/js/vcenter.js
@@ -1,6 +1,8 @@
-var textblocks = document.getElementsByClassName('inset');
-var sectionHeight = 480;
-for(var i = 0; i < textblocks.length; i++) {
-    var offset = (sectionHeight - textblocks[i].clientHeight) / 2;
-    textblocks[i].style.marginTop = offset + 'px';
-}
+(function() {
+    var textblocks = document.getElementsByClassName('inset');
+    var sectionHeight = 480;
+    for(var i = 0; i < textblocks.length; i++) {
+        var offset = (sectionHeight - textblocks[i].clientHeight) / 2;
+        textblocks[i].style.marginTop = offset + 'px';
+    }
+}())
